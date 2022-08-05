270e768f14b55ee47817b26953cb2c9a50ebb892
Samara <samacard15@hotmail.com>
2022-08-05 17:37:52 -0300

    req 3

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Game from './pages/Game';
 import Login from './pages/Login';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" render={ (props) => <Login { ...props } /> } />
       <Route path="/games" component={ Game } />
+      <Route path="/settings" component={ Settings } />
     </Switch>
   );
 }
