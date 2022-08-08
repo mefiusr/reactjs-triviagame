a2a73ffdddb2a7d22036180f82921033d1abc37b
Mathews <mathewsrodrigues9@gmail.com>
2022-08-08 16:06:08 -0300

    iniciando req7

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,10 +1,28 @@
+import styled from 'styled-components';
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import '../styles/style.css';
 
 class Game extends Component {
+  buttonGreen = styled.button`
+  border: 10px solid rgb(6, 240, 15);
+  `;
+
+  buttonRed = styled.button`
+  border: 3px solid red;
+  `;
+
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <button
+          type="button"
+          onClick={ () => this.addStyle() }
+        >
+          TESTE
+        </button>
+      </>
     );
   }
 }
