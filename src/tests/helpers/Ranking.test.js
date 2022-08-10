3450871cd1cc5d5f8af97d8a8d2f7584c4f18edf
Mathews <mathewsrodrigues9@gmail.com>
2022-08-10 17:43:22 -0300

    req 20

--- src/tests/helpers/Ranking.test.js
+++ src/tests/helpers/Ranking.test.js
@@ -28,4 +28,33 @@ describe('Testes no componente Ranking', () => {
     userEvent.click(button);
     expect(history.location.pathname).toBe('/');
   })
+
+  it('Testa se ao clicar no botão login, a página é redirecionada e o sort é testado', () => {
+
+    const string = JSON.stringify({result: [{ name: 'samara', score: 1, picture: `https://opentdb.com/api.php?amount=5&token={img}` },
+    { name: 'samara', score: 2, picture: `https://opentdb.com/api.php?amount=5&token={img}` },
+    { name: 'samara', score: 3, picture: `https://opentdb.com/api.php?amount=5&token={img}` },
+    { name: 'samara', score: 4, picture: `https://opentdb.com/api.php?amount=5&token={img}` },
+    { name: 'samara', score: 0, picture: `https://opentdb.com/api.php?amount=5&token={img}` }]},
+    )
+
+    localStorage.setItem('ranking', string);
+
+    const initialState = {
+      player: {
+        name: 'samara',
+        assertions: 2,
+        score: 25,
+        gravatarEmail: 'samara@trybe.com',
+        questions: [],
+        firstLogin: true,
+      }
+    };
+
+    const { history } = renderWithRouterAndRedux(<App />, initialState, '/ranking');
+
+    const button = screen.getByTestId('btn-go-home');
+    userEvent.click(button);
+    expect(history.location.pathname).toBe('/');
+  })
 })
